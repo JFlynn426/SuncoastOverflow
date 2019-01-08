651a015acc75cc9b25a6edbcc0ab27bae6e6a161
Frank Diaz <diazfrankp@gmail.com>
2019-01-08 16:55:23 -0500

    Add text area to questions page.

--- ClientApp/src/components/Questions.js
+++ ClientApp/src/components/Questions.js
@@ -16,14 +16,7 @@ export class Questions extends Component {
             <button>Vote</button>
           </div>
           <div className="Questions">
-            <p>
-              Lorem Ipsum Questions: I find demos on how to debug .net core apps
-              in Visual Studio everywhere but not for Angular and VS Code. The
-              only thing I see out there is how to deploy an Angular app using
-              VS Code, Docker, Kubernetes, etc. Nothing for debugging in VS
-              Code. Is this possible? If so, can someone direct me on how to
-              configure VS Code to allow me to do this?
-            </p>
+            <textarea id="story" name="story" rows="5" cols="33" />
 
             <div className="CreatedTimeAndAuthor">
               <div>Modified 2 minutes ago</div>
