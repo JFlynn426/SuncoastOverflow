53745c429319b2c6cc1975a69b51b8a85a99597b
Frank Diaz <diazfrankp@gmail.com>
2019-01-08 18:28:38 -0500

    Further HTML and CSS. Answers page has input field.

--- ClientApp/src/components/Answers.js
+++ ClientApp/src/components/Answers.js
@@ -5,23 +5,75 @@ export class Answers extends Component {
 
   render() {
     return (
-      <div className="Titles">
-        <div className="VoteButton">
-          <button>Vote</button>
+      <div>
+        <h4>Question Title</h4>
+        <div className="PostedQuestion">
+          <div className="VoteButton">
+            <button className="VoteButton">Vote Up</button>
+            <button className="VoteButton">Vote Down</button>
+          </div>
+          <div className="Question">
+            <p>
+              Lorem Ipsum QUESTION: I find demos on how to debug .net core apps
+              in Visual Studio everywhere but not for Angular and VS Code. The
+              only thing I see out there is how to deploy an Angular app using
+              VS Code, Docker, Kubernetes, etc. Nothing for debugging in VS
+              Code. Is this possible? If so, can someone direct me on how to
+              configure VS Code to allow me to do this?
+            </p>
+            <div className="CreatedTimeAndAuthor">
+              <div>Modified 2 minutes ago</div>
+              <div>Author's name</div>
+            </div>
+          </div>
         </div>
-        <div className="AskQuestion">
-          <p>
-            Lorem Ipsum AskQuestion: I find demos on how to debug .net core apps
-            in Visual Studio everywhere but not for Angular and VS Code. The
-            only thing I see out there is how to deploy an Angular app using VS
-            Code, Docker, Kubernetes, etc. Nothing for debugging in VS Code. Is
-            this possible? If so, can someone direct me on how to configure VS
-            Code to allow me to do this?
-          </p>
+        <h4>Answers</h4>
+        <div className="PostedAnswer">
+          <div className="VoteButton">
+            <button className="VoteButton">Vote Up</button>
+            <button className="VoteButton">Vote Down</button>
+          </div>
+          <div className="Answer">
+            <p>
+              Lorem Ipsum ANSWER: I find demos on how to debug .net core apps in
+              Visual Studio everywhere but not for Angular and VS Code. The only
+              thing I see out there is how to deploy an Angular app using VS
+              Code, Docker, Kubernetes, etc. Nothing for debugging in VS Code.
+              Is this possible? If so, can someone direct me on how to configure
+              VS Code to allow me to do this?
+            </p>
+            <div className="CreatedTimeAndAuthor">
+              <div>Modified 2 minutes ago</div>
+              <div>Author's name</div>
+            </div>
+          </div>
+        </div>
+        <h4>Post Your Answer</h4>
+        <div className="Header">
+          <textarea
+            id="story"
+            name="story"
+            rows="1"
+            cols="40"
+            placeholder="Title"
+          />
+          <button>Submit Answer</button>
+        </div>
 
-          <div className="CreatedTimeAndAuthor">
-            <div>Modified 2 minutes ago</div>
-            <div>Author's name</div>
+        <div className="Titles">
+          <div className="Questions">
+            <textarea
+              id="story"
+              name="story"
+              rows="10"
+              cols="40"
+              placeholder="Answer"
+            />
+
+            <div class="CreatedTimeAndAuthor">
+              <div>Modified 2 minutes ago</div>
+              <input type="text" placeholder="Author" />
+            </div>
           </div>
         </div>
       </div>
--- ClientApp/src/components/Questions.js
+++ ClientApp/src/components/Questions.js
@@ -7,20 +7,29 @@ export class Questions extends Component {
     return (
       <div>
         <div className="Header">
-          <h1>Question Title 1</h1>
+          <textarea
+            id="story"
+            name="story"
+            rows="2"
+            cols="40"
+            placeholder="Title"
+          />
           <button>Ask Question</button>
         </div>
 
         <div className="Titles">
-          <div className="VoteButton">
-            <button>Vote</button>
-          </div>
           <div className="Questions">
-            <textarea id="story" name="story" rows="5" cols="33" />
+            <textarea
+              id="story"
+              name="story"
+              rows="10"
+              cols="40"
+              placeholder="Body"
+            />
 
             <div class="CreatedTimeAndAuthor">
               <div>Modified 2 minutes ago</div>
-              <input type="text" />
+              <input type="text" placeholder="Author" />
             </div>
           </div>
         </div>
